b91c630a71a1e55ea700fb3201114cbf284aecd5
joneskj55 <joneskj55@gmail.com>
2021-08-30 19:00:24 -0500

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+/*
+============================================
+; Title:  app.component.ts
+; Author: Professor Krasso
+; Modified by: Kevin Jones
+; Date: 30 Aug 2021
+; Description: App component TS file
+;===========================================
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
-  styles: [``]
+  styles: [``],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
